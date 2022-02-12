5dcd73df3a7725d384ff3de253366edee6292ca3
SaMiAlex <samias@mail.ru>
2022-02-12 11:51:22 +0600

    Добавьте файлы проекта.

---
+++ CreationModelPlugin/LevelUtil.cs
@@ -0,0 +1,26 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationModelPlugin
+{
+    public class LevelUtil
+    {
+
+        public static List<Level> GetLevels(ExternalCommandData commandData)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            List<Level> listlevel = new FilteredElementCollector(doc)
+                    .OfClass(typeof(Level))
+                    .OfType<Level>()
+                    .ToList();
+
+            return listlevel;
+        }
+    }
+}
